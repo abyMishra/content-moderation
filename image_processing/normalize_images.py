145c5c88b3a0e52dda76d1d9abd76d70ed815ba9
Brandon Dang <frogninja@sbcglobal.net>
2017-10-18 23:30:13 -0500

    adding image processing code

---
+++ image_processing/normalize_images.py
@@ -0,0 +1,47 @@
+import json
+import os
+
+labels = {}
+count = 1000
+
+try:
+  os.removedirs('images')
+except:
+  pass
+
+os.mkdir('images')
+
+for dirname, dirnames, filenames in os.walk('.'):
+    dirname = dirname[2:]
+    og_dirname = dirname
+
+    if 'images' in dirname or dirname is '':
+        continue
+
+    if dirname[-1:].isdigit():
+        dirname = dirname[:-1]
+
+    os.mkdir('images/' + dirname)
+
+    for f in filenames:
+        if (f.endswith('gif')):
+          os.remove(f)
+
+        new_f = str(count) + '.jpg'
+        count += 1
+
+        os.rename(og_dirname + '/' + f,
+                  'images/' + dirname + '/' + new_f)
+
+        image_label, image_type = dirname.split('_')
+        labels[new_f] = {
+            'label': image_label,
+            'type': image_type,
+            'label_type': dirname
+        }
+
+    os.removedirs(og_dirname)
+
+with open('true_labels.json', 'w') as handle:
+    json.dump(labels, handle)
+
