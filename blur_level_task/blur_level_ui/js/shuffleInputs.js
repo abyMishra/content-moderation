bc784e07c04b34936301d99973471cf9811bdbd2
Brandon Dang <frogninja@sbcglobal.net>
2017-10-23 16:48:28 -0500

    randomizing radio button order

---
+++ blur_level_task/blur_level_ui/js/shuffleInputs.js
@@ -0,0 +1,30 @@
+var categories =
+  ["<input type='radio' name='category' value='pornography'> Pornography<br>", 
+  "<input type='radio' name='category' value='violence'> Violence<br>", 
+  "<input type='radio' name='category' value='sfw'> Safe for work (SFW)<br>"]
+
+var types = 
+  ["<input type='radio' name='type' value='realistic'> Yes<br>",
+  "<input type='radio' name='type' value='synthetic'> No<br>"]
+
+function shuffleInputs() {
+  shuffle(categories);
+  shuffle(types);
+
+  $("#categories").empty();
+  $("#categories").append(categories.join("\n"));
+
+  $("#types").empty();
+  $("#types").append(types.join("\n"));
+}
+
+/* https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array*/
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+}
