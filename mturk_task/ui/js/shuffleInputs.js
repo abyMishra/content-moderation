c8b2e1a5c0f31e6c2906c682f0b67e615fc169f5
budang <frogninja@sbcglobal.net>
2017-11-16 23:58:08 -0600

    adding a tooltip for each image category in the task; minor HTML touch ups

--- mturk_task/ui/js/shuffleInputs.js
+++ mturk_task/ui/js/shuffleInputs.js
@@ -1,7 +1,17 @@
+var pornDef = "This image displays pornography, nudity, or overly sexual themes.";
+var violDef = "This image displays excessive violence, torture, gore, blood, or injury towards a person or animal.";
+var safeDef = "This image would be ok to comfortably show a 10-year-old, your boss, or your coworkers.";
+
 var categories =
-  ["<input type='radio' name='category' value='pornography'> Pornography<br>",
-  "<input type='radio' name='category' value='violence'> Violence / Gore<br>",
-  "<input type='radio' name='category' value='safe'> Safe for general audiences<br>"];
+  ["<input type='radio' name='category' value='pornography'> Pornography "
+    + "<a href='#' data-toggle='tooltip' data-placement='right' title='" + pornDef + "'>"
+    + "<span class='glyphicon glyphicon-question-sign'></span></a><br>",
+  "<input type='radio' name='category' value='violence'> Violence / Gore "
+    + "<a href='#' data-toggle='tooltip' data-placement='right' title='" + violDef + "'>"
+    + "<span class='glyphicon glyphicon-question-sign'></span></a><br>",
+  "<input type='radio' name='category' value='safe'> Safe for general audiences "
+    + "<a href='#' data-toggle='tooltip' data-placement='right' title='" + safeDef + "'>"
+    + "<span class='glyphicon glyphicon-question-sign'></span></a><br>"];
 
 var types =
   ["<input type='radio' name='type' value='realistic'> Yes<br>",
@@ -26,6 +36,9 @@ function shuffleInputs() {
   $("#approval").empty();
   $("#approval").append(approval.join("\n"));
 
+  $(document).ready(function() {
+    $("[data-toggle='tooltip']").tooltip();
+  });
 }
 
 /* https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array*/
