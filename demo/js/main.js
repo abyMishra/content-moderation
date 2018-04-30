8ca535b2005effa8ba95f0e7352c677ec4de1a9d
budang <frogninja@sbcglobal.net>
2018-04-29 22:23:16 -0500

    updating button interface to be more intuitive

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var buttons = $("button");
+
   $("#slider").css("display", "none");
 
   $(".accordion").accordion({
@@ -11,31 +13,38 @@ $(document).ready(function() {
   $("button").click(function() {
     var id = $(this).attr("id");
 
+    $("button").each(function(idx) {
+      $(this)
+        .removeClass("btn-warning")
+        .addClass("btn-primary");
+    });
+
+    $(this)
+      .addClass("btn-warning")
+      .removeClass("btn-primary");
+
     if (id == "click") {
+      $("#slider").css("display", "none");
       blurOptns = {
         stdDeviation: 14,
         sclick: {
           circle: { r: 50 }
         }
       }
-
-      $("#slider").css("display", "none");
     } else if (id == "mouse") {
+      $("#slider").css("display", "none");
       blurOptns = {
         stdDeviation: 14,
         smouseover: {
           circle: { r: 50 }
         }
       }
-
-      $("#slider").css("display", "none");
     } else if (id == "slide") {
+      $("#slider").css("display", "block");
+      $("#slider").val(14);
       blurOptns = {
         stdDeviation: 14
       }
-
-      $("#slider").css("display", "block");
-      $("#slider").val(14);
     }
 
     $("img").jqImgBlur(blurOptns);
